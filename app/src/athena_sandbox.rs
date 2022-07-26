3019a28f4c18c7f4692d2013810526182e22adc5
Tannr <tannr@hey.com>
2022-07-26 14:38:42 -0400

    Wait on sandbox execution & return sb output to frontend output UI

--- app/src/athena_sandbox.rs
+++ app/src/athena_sandbox.rs
@@ -1,12 +1,18 @@
-use std::{path::{PathBuf, Path}, ffi::OsString};
+use std::{
+    ffi::OsString,
+    io::Read,
+    path::{Path, PathBuf},
+    process::Stdio,
+};
 
-use tokio::process::Command;
-use serde::{Serialize, Deserialize};
+use serde::{Deserialize, Serialize};
 use tempfile::{self, TempDir};
+use tokio::io::{self, AsyncReadExt};
+use tokio::process::Command;
+use tokio_stream::StreamExt;
 use uuid::Uuid;
+
 use crate::Config;
-
-
 
 #[derive(Deserialize, Default)]
 pub struct AthenaFileInput {
@@ -15,15 +21,13 @@ pub struct AthenaFileInput {
     pub name: String,
 }
 
-
 pub struct Sandbox {
     file: AthenaFileInput,
     container_id: Option<String>,
-    dir: TempDir
+    dir: TempDir,
 }
 
 impl Sandbox {
-
     pub fn athfile(&self) -> PathBuf {
         self.dir.path().join(self.file.name())
     }
@@ -37,24 +41,22 @@ impl Sandbox {
     }
 
     pub async fn new(f: AthenaFileInput) -> Self {
-        let temp_dir = tempfile::Builder::new()
-            .prefix("temp-ath")
-            .tempdir();
+        let temp_dir = tempfile::Builder::new().prefix("temp-ath").tempdir();
         match temp_dir {
-            Ok(d) => {
-                Self { file: f, container_id: None, dir: d }
-            }
+            Ok(d) => Self {
+                file: f,
+                container_id: None,
+                dir: d,
+            },
             Err(e) => {
                 println!("ERROR {:?}", e);
                 Self {
                     file: f,
                     container_id: None,
-                    dir: TempDir::new().unwrap()
+                    dir: TempDir::new().unwrap(),
                 }
             }
         }
-       
-
     }
     pub async fn write_ath_module(&self) {
         let src = self.file.src_code();
@@ -64,16 +66,21 @@ impl Sandbox {
             .expect("Error writing athena code to temp file");
     }
 
+    pub fn container_id_from_output(&mut self, out: std::process::Output) {
+        let output_str = String::from_utf8_lossy(&out.stdout);
+        let id = output_str.lines().next().unwrap().trim();
+
+        self.container_id = Some(id.to_string());
+    }
 
     pub fn generate_run_command(&self) -> Command {
         let mut cmd = Command::new("docker");
-            
-            let mut mount_exec_file = self.athfile_with_ext().into_os_string();
-            mount_exec_file.push(":");
-            mount_exec_file.push("/athena/temp-ath-files/");
-            mount_exec_file.push(self.file.name_with_ext());
-            cmd
-            .arg("run")
+
+        let mut mount_exec_file = self.athfile_with_ext().into_os_string();
+        mount_exec_file.push(":");
+        mount_exec_file.push("/athena/temp-ath-files/");
+        mount_exec_file.push(self.file.name_with_ext());
+        cmd.arg("run")
             .arg("--name")
             .arg(format!("{}", self.file.name()))
             .arg("--detach")
@@ -84,15 +91,59 @@ impl Sandbox {
             .arg("--volume")
             .arg(mount_exec_file)
             .arg("athena_runtime");
-            
+
         cmd.kill_on_drop(true);
         cmd
     }
 
     pub fn execute(&self, cmd: &mut Command) {
-            cmd.arg(format!("/athena/temp-ath-files/{}.ath", self.file.name()));
-            // cmd.arg("--env")
-            //     .arg(format!("ATH_FILE_NAME={}", self.name));
+        cmd.arg(format!("/athena/temp-ath-files/{}.ath", self.file.name()));
+        // cmd.arg("--env")
+        //     .arg(format!("ATH_FILE_NAME={}", self.name));
+    }
+
+    // Primary way to execute a command. This waits for the container to complete and collects its output
+    pub async fn wait_on_cmd(&mut self, mut cmd: Command) -> String {
+        let output = cmd.output().await.expect("Error executing command");
+
+        self.container_id_from_output(output);
+        Command::new("docker")
+            .arg("wait")
+            .arg(self.container_id.as_ref().unwrap())
+            .output()
+            .await
+            .expect("Error waiting on container execution");
+
+        self.get_logs().await
+    }
+
+    pub async fn shutdown(&self) {
+        let mut command = Command::new("docker");
+
+        command
+            .arg("rm")
+            .arg("--force")
+            .arg(self.container_id.as_ref().unwrap())
+            .stdout(std::process::Stdio::null());
+        command
+            .status()
+            .await
+            .expect("Error removing container during shutdown");
+    }
+    pub async fn get_logs(&self) -> String {
+        let mut cmd = std::process::Command::new("docker");
+        cmd.arg("logs").arg(&self.container_id.as_ref().unwrap());
+        let outp = cmd.stdout(Stdio::piped()).output();
+
+        match outp {
+            Ok(o) => {
+                let o = String::from_utf8_lossy(&o.stdout);
+                o.to_string()
+            }
+            Err(e) => {
+                format!("Error in log retrieval: {:#?}", e)
+            }
+        }
     }
 }
 impl AthenaFileInput {
@@ -114,13 +165,10 @@ impl AthenaFileInput {
     pub fn src_code(&self) -> &str {
         &self.ath
     }
-
 }
 
 #[derive(Serialize)]
 pub struct AthenaExecResult {
     pub err: bool,
-    pub message: String
+    pub message: String,
 }
-
-
--- app/src/main.rs
+++ app/src/main.rs
@@ -1,5 +1,5 @@
-mod server;
 pub(crate) mod athena_sandbox;
+mod server;
 use std::{env, net::SocketAddr, path::PathBuf};
 const DEFAULT_ADDRESS: &str = "127.0.0.1";
 const DEFAULT_PORT: u16 = 3000;
@@ -12,21 +12,27 @@ pub struct Config {
 
 impl Config {
     fn addr(&self) -> SocketAddr {
-        let addr = self.address.parse().expect("Unable to parse socket address");
+        let addr = self
+            .address
+            .parse()
+            .expect("Unable to parse socket address");
         SocketAddr::new(addr, self.port)
     }
 }
 
 impl Default for Config {
     fn default() -> Self {
-        Self { address: DEFAULT_ADDRESS.to_string(), port: DEFAULT_PORT, ath_file_path: PathBuf::from(DEFAULT_ATH_FILE_PATH) }
+        Self {
+            address: DEFAULT_ADDRESS.to_string(),
+            port: DEFAULT_PORT,
+            ath_file_path: PathBuf::from(DEFAULT_ATH_FILE_PATH),
+        }
     }
 }
 
-
 fn main() {
     let mut config = Config::default();
-    config.ath_file_path =  PathBuf::from(env!("CARGO_MANIFEST_DIR")).join("temp-ath-files");
+    config.ath_file_path = PathBuf::from(env!("CARGO_MANIFEST_DIR")).join("temp-ath-files");
 
     server::serve(config);
 }
