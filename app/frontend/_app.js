d3700490f420cf736d1a1a3fb986699937ffd383
Tannr <tannr@hey.com>
2022-08-17 01:35:08 -0400

    diff default examples

--- app/frontend/_app.js
+++ app/frontend/_app.js
@@ -170,55 +170,137 @@ export default function Home() {
 // });
 
 function getCode() {
-	return `module Nat {
-    datatype N := zero | (S N)
-
-    declare +: [N N] -> N  [200] # precedence 200
-    declare *: [N N] -> N  [300] # precedence 300
-    declare **: [N N] -> N [400] # precedence 400
-
-    declare Nul: [N] -> N
-
-    # Some variables to use in definitions & proofs
-    define [n m x y] := [?n:N ?m:N ?x:N ?y:N]
-    define one := (S zero)
-  
-  
-    # Universally quantified definitions of addition, multiplication and exponentiation
-    assert* [ 
-             (n + S m = S (n + m))
-             (n + zero = n)
-             (x * zero = zero)
-             (x * (S y) = x * y + x)
-             (x ** zero = one)
-             (x ** S n = x * x ** n)  
-    ]
-
-    # Definitions of Nul function
-    assert* nul-axioms := [(Nul S n = Nul n) (Nul zero = zero)]
-    define [nul-n nul-z] := nul-axioms
+	return `module Example {
+    # Welcome to the Athena Playground!
+    # Run any of the examples in here, or try writing some proofs, yourself!
 
 
-    # 8 ** 3
-    define eight-pow-3 := (Nul (** (S (S (S (zero))))  (S (S (S (S (S (S (S (S zero))))))))))
+    # Athena comes with many modules in its default libraries.
+    # You can access those via 'load "lib-name"'. The full list of library files
+    # can be found here https://github.com/AthenaFoundation/athena/tree/master/listings/lib
 
+    # Below, you will find three examples: 
+    # 1. A derivation of asymmetry
+    # 2. An example of executing operations via rewrite rules
+    # 3. An example of using Athena's integrations with external ATP's
+    # 4. An extended example with rewriting and proofs by induction
 
+    # Note: Running all examples at once may cause the sandbox's metering to kick in
+    # If this happens, just comment-out or delete whichever example(s) are not of interest
+    # Each example should complete within a few seconds
 
-    # Proof that forall Natural numbers, Nul n = zero
-    by-induction (forall n . Nul n = zero) {
-        zero => (!chain-> [
-            (Nul zero) = zero [nul-z]
-        ])
-        | (S n) => let {ind-hypothesis := (Nul n = zero)}
-                        conclude conc := (Nul (S n) = zero)
-                            (!chain-> [
-                                (Nul S n) = (Nul n) [nul-n]
-                                          = zero [ind-hypothesis]
-                            ])
+    module AsymmetryExample {
+      (print "-------Asymmetry Example----------")
+        domain D
+        declare <: [D D] -> Boolean
+      
+        define [x y z] := [?x:D ?y:D ?z:D]
+      
+        assert* irreflexivity := (~ x < x)
+        assert* transitivity := (x < y & y < z ==> x < z)
+      
+        conclude asymmetry := (forall x y . x < y ==> ~ y < x)
+          pick-any a:D b:D
+            assume (a < b)
+              (!by-contradiction (~ b < a)
+                assume (b < a) 
+                  let {less := (!chain-> [(b < a) 
+                                      ==> (a < b & b < a)    [augment]
+                                      ==> (a < a)            [transitivity]]);
+                       not-less := (!chain-> [true 
+                                          ==> (~ a < a)      [irreflexivity]])}
+                    (!absurd less not-less))
     }
 
-    # Proof that Nul 8 ** 3 = zero
-    (!chain  [eight-pow-3 = zero [(forall n . Nul n = zero)] ])
+    module AutomatedProofExample {
+      (print "-------Automated Proof Example----------")
+      declare A,B,C,D: Boolean
+      assume h1 := (A & B)
+      assume h2 := (~C ==> ~B)
+      (!prove C [h1 h2])
+    }
+
+    module ExampleRewrite {
+      (print "-------Rewrite Example----------")
+      
+      load "nat-plus"
+       
+      define + := N.+
+        
+      # Procedures defined in nat-plus to convert
+      # integers to inductively defined natural numbers
+      define one := (int->nat 1)
+      define two := (int->nat 2)
+      define three := (int->nat 3)
+      define five := (int->nat 5)
+  
+      
+      # Pretty print the successor number
+      transform-output eval [nat->int]
+      (eval two + three) # should be 5
+  
+      # prove 2 + 3 = 5 using the Successor representation of N
+      conclude goal := ((two + three) = five)
+      (!chain [
+        (two + three)
+                  = (S (two + two))          
+                  = (S S (two + one))        
+                  = (S S S (two + zero))     
+                  = (S S S two)             
+                  = five                     
+      ])
+  
+    }
+    module NullifyNumbersExample {
+      (print "-------Nullify Numbers Example----------")
+      datatype N := zero | (S N)
+
+      declare +: [N N] -> N  [200] # precedence 200
+      declare *: [N N] -> N  [300] # precedence 300
+      declare **: [N N] -> N [400] # precedence 400
+
+      declare Nul: [N] -> N
+
+      # Some variables to use in definitions & proofs
+      define [n m x y] := [?n:N ?m:N ?x:N ?y:N]
+      define one := (S zero)
     
+    
+      # Universally quantified definitions of addition, multiplication and exponentiation
+      assert* [ 
+              (n + S m = S (n + m))
+              (n + zero = n)
+              (x * zero = zero)
+              (x * (S y) = x * y + x)
+              (x ** zero = one)
+              (x ** S n = x * x ** n)  
+      ]
+
+      # Definitions of Nul function
+      assert* nul-axioms := [(Nul S n = Nul n) (Nul zero = zero)]
+      define [nul-n nul-z] := nul-axioms
+
+
+      # 8 ** 3
+      define eight-pow-3 := (Nul (** (S (S (S (zero))))  (S (S (S (S (S (S (S (S zero))))))))))
+
+
+
+      # Proof that forall Natural numbers, Nul n = zero
+      by-induction (forall n . Nul n = zero) {
+          zero => (!chain-> [
+              (Nul zero) = zero [nul-z]
+          ])
+          | (S n) => let {ind-hypothesis := (Nul n = zero)}
+                          conclude conc := (Nul (S n) = zero)
+                              (!chain-> [
+                                  (Nul S n) = (Nul n) [nul-n]
+                                            = zero [ind-hypothesis]
+                              ])
+      }
+
+      # Proof that Nul 8 ** 3 = zero
+      (!chain  [eight-pow-3 = zero [(forall n . Nul n = zero)] ])
+    }
 }`
 }
